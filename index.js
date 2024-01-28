64b33f08bce5849940482b07c78f70bb7567cdbe
Mohamed Jubair <mohamed.jubair@livspace.com>
2024-01-28 14:30:11 +0530

    Connect to mongodb using mongoose

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(
+    'mongodb+srv://mjubair:x0XxlbNpdMzo6Bi7@mjubair.wo1nakp.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Connected to MongoDB');
+  });
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({ status: 'ok', data: { message: 'Hello World!' } });
 });
+
+app.post('/register', (req, res) => {
+  res.json({ status: 'ok', data: { message: 'Hello World!' } });
+});
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
